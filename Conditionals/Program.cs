aca7ab86523388e7c3384591a8f76c30caa354ac
Birsu Çaylak <birsusalli@gmail.com>
2023-10-23 23:00:55 +0300

    If Bloklarıyla Çalışmaya Başlamak

---
+++ Conditionals/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 10;
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+             }
+            else
+            {
+                Console.WriteLine("Number is not 10");
+            }
+            Console.ReadLine();
+        }
+    }
+}
