d08d7242edd5a3b9eeb0ee7512e6f6f5441ddc1e
Birsu Çaylak <birsusalli@gmail.com>
2023-10-23 20:17:14 +0300

    Integer Veri Tipi

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1 = 10;
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
