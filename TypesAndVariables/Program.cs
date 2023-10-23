935d0f16a0fed355b859e2e96aaca82f058073ef
Birsu Çaylak <birsusalli@gmail.com>
2023-10-23 21:56:43 +0300

    Double Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -7,6 +7,7 @@ namespace TypesAndVariables
         static void Main(string[] args)
         {
 
+            double number5 = 10.4;
             char character = 'A';
             bool condition = false;
             byte number4 = 255;
@@ -17,6 +18,7 @@ namespace TypesAndVariables
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
             Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
             Console.WriteLine("Character is : {0}", character);
             Console.ReadLine();
 
