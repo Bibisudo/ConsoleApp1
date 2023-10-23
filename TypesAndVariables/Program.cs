d323f390075310db31ae3bce3971de6c673e338c
Birsu Çaylak <birsusalli@gmail.com>
2023-10-23 20:52:45 +0300

    Long Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -8,7 +8,9 @@ namespace TypesAndVariables
         {
 
             int number1 = 10;
+            long number2 = 264846549846;
             Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
             Console.ReadLine();
         }
     }
