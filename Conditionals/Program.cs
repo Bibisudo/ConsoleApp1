bac1de9ead0a62b88c88537668a8871839ef68c4
Birsu Çaylak <birsusalli@gmail.com>
2023-10-24 01:35:42 +0300

    Else If Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -4,10 +4,20 @@
     {
         static void Main(string[] args)
         {
-            var number = 11;
-            Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
-            Console.ReadLine();
+            var number = 30;
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+             }
+            else if (number == 20) 
+            {
+                Console.WriteLine("Number is 20");
+            }
 
+            else {
+                Console.WriteLine("Number is not 10 or 20");
+            }
+            Console.ReadLine();
         }
     }
 }
