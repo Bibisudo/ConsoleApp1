863bed87fed528b399abd5ee98673ecff62c3a27
Birsu Çaylak <birsusalli@gmail.com>
2023-10-23 21:51:20 +0300

    Byte veri tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -7,12 +7,14 @@ namespace TypesAndVariables
         static void Main(string[] args)
         {
 
+            byte number4 = 255;
             short number3 = 32767;
             int number1 = 10;
             long number2 = 264846549846;
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
             Console.ReadLine();
 
         }
