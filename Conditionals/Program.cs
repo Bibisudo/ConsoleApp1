3904afcd3aa34de0c0163423533cbcc5d8d6996f
Birsu Çaylak <birsusalli@gmail.com>
2023-10-24 01:31:38 +0300

    Single Line If İle Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -4,16 +4,15 @@
     {
         static void Main(string[] args)
         {
-            var number = 10;
-            if (number == 10)
-            {
-                Console.WriteLine("Number is 10");
-             }
-            else
-            {
-                Console.WriteLine("Number is not 10");
-            }
+            var number = 11;
+            Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
             Console.ReadLine();
+
         }
     }
 }
+
+
+           
+    
+
