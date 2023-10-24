cc33b44f779d5c0e7c686752d263c6a6e64f3080
Birsu Çaylak <birsusalli@gmail.com>
2023-10-24 21:01:29 +0300

    For Döngüsü

---
+++ Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
+
+
+            for (int i = 1; i <= 100; i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
+        } 
+    }
+}
