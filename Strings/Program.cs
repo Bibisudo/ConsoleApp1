9b1d4ba93723944defd061aa41b27f9b60d913ae
Birsu Çaylak <birsusalli@gmail.com>
2023-10-24 23:38:32 +0300

    String Metodlarıyla Çalışmak

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -6,16 +6,36 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            string city = "Ankara";
-            //Console.WriteLine(city[0]);
+            //  string city = "Ankara";
+            //  //Console.WriteLine(city[0]);
 
-          foreach (var item in city)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in city)
+            //  {
+            //      Console.WriteLine(item);
+            //  }
 
-            string city2 = "İstanbul";
-            Console.WriteLine(String.Format("{0} {1}", city, city2 ));
+            //  string city2 = "İstanbul";
+            //  Console.WriteLine(String.Format("{0} {1}", city, city2 ));
+
+            string sentence = "My name is Birsu";
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            sentence = "My name is Engin Demiroğ";
+           
+            bool result3 = sentence.EndsWith("r");
+            bool result4 = sentence.StartsWith("My name");
+            var result5 = sentence.IndexOf("name");
+            var result6 = sentence.IndexOf(" ");
+            var result7 = sentence.IndexOf(" ");
+            var result8 = sentence.Insert(0, "Hello,");
+            var result9 = sentence.Substring(3);
+            var result10 = sentence.ToLower();
+            var result11 = sentence.ToUpper();
+            var result12 = sentence.Replace(" ", "-");
+            var result13 = sentence.Remove(2,4);
+
+
+            Console.WriteLine(result9);
                 
 
             Console.ReadLine();
