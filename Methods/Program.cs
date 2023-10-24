4ee9507343489e48966be0174a83307bb872458d
Birsu Çaylak <birsusalli@gmail.com>
2023-10-24 11:26:44 +0300

    Default Parametreli Metotlarla Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Add();
-            var result = Add2(20, 30);
+            var result = Add2(20);
             Console.WriteLine(result);
             Console.ReadLine();
         }
@@ -13,7 +13,8 @@
         { Console.WriteLine("Added!");
         
         }
-        static int Add2(int number1, int number2) 
+        static int Add2(int number1, int number2=30) 
+            // Default değerler metodun en sonunda olması gerekiyor.
         { 
             var result = number1 + number2;
             return result;
