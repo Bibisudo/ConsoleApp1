fbd0f402408e95d5c1781b354a9fbbee745494d5
Birsu Çaylak <birsusalli@gmail.com>
2023-10-26 15:17:28 +0300

    Lottery Project

---
+++ Lottery2/Program.cs
@@ -0,0 +1,36 @@
+﻿int columnNumber;
+Console.Write("Kolon sayısı giriniz:");
+columnNumber= int.Parse(Console.ReadLine());
+
+if (columnNumber > 0 && columnNumber < 8)
+{
+Random random = new Random();
+    int[] column = new int[6];
+    for (int i = 0; i < columnNumber; i++) // i eğer columnNumber'dan küçükse sen bu foru çalıştır. koışul yazdığın yer ortası.
+    {
+        for (int j = 0; j < column.Length; j++)
+        {   
+            int randomNumber = random.Next(1,49);
+            while (column.Contains(randomNumber))
+            {
+                randomNumber = random.Next(1, 49);
+            }
+            column[j] = randomNumber;
+
+        }
+        foreach (int number in column)
+        {
+            Console.Write(number+" ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("***********");
+    }
+}
+else
+{
+    Console.WriteLine("Hatasız kul olmaz! Bir daha dene!");
+}
+
+
+
+
