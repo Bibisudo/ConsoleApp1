b2dd794ec7cec4788531b2d5f1c0f1d83548516a
Birsu Çaylak <birsusalli@gmail.com>
2023-10-26 23:12:32 +0300

    Gerçek Hayatta İnterface Kullanımı

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -4,74 +4,10 @@
     {
         static void Main(string[] args)
         {
-            PersonManager manager = new PersonManager();
-            Customer customer = new Customer
-            {
-                Id = 1,
-                FirstName = "Engin",
-                LastName = "Demirog",
-                Address = "Ankara"
-            };
+          
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
 
-
-            Student student = new Student
-            {
-                Id = 1,
-                FirstName = "Derin",
-                LastName = "Demirog",
-                Departmant = "Computer Sciences"
-            };
-
-            manager.Add(student);
-            manager.Add(customer);
-
-
-        }
-
-
-
-    }
-    
-    interface IPerson
-    {
-        int Id { get; set; }
-        string FirstName { get; set; }
-        string LastName { get; set; }
-
-    }
-
-    
-    class Customer : IPerson
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public string Address { get; set; }
-    }
-
-    class Student : IPerson
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public string Departmant { get; set; }
-    }
-
-    class Worker : IPerson
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public string Departmant { get; set; }
-    }
-    class PersonManager
-    {
-        public void Add(IPerson person)
-        {
-            Console.WriteLine(person.FirstName);
         }
     }
 }
